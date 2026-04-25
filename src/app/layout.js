311c177caa3bd008510744f3683e8f804440a891
Hrithic <hrithicraj730@gmail.com>
2026-04-25 14:55:09 +0530

    change: favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Machine Task Collector',
   description: 'Collect and search machine tasks from companies',
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
